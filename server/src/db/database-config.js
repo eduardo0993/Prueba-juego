75e87471c83f0fb440c8d5a90a4666102c6a4587
eduardo0993 <122618079+eduardo0993@users.noreply.github.com>
2023-04-10 17:07:01 -0500

    conexion a base de datos y variables de entorno y puerto del server

---
+++ server/src/db/database-config.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+const url=process.env.DBSERVERLOCAL
+
+const dbConexion = async () => {
+    try {
+        mongoose.set("strictQuery", false);
+        mongoose.connect(url);
+        console.log("Conexion a la DB exitosa");
+    } catch (error) {
+     throw new Error("Erroe de conexion, comunicate con el administrador");
+    }
+}
+
+module.exports={
+    dbConexion
+};
