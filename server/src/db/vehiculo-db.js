a5730332f25a42a579483433d433049fdcfe5efa
eduardo0993 <122618079+eduardo0993@users.noreply.github.com>
2023-04-11 17:08:59 -0500

    servicio registro de vehiculo sin terminar

---
+++ server/src/db/vehiculo-db.js
@@ -0,0 +1,25 @@
+const vehiculo = require("../modelos-db/vehiculo-modelo");
+
+const crearVehiculo = async(car) =>{
+    const use = new vehiculo({
+        nombre:car.nombre,
+        tipo:car.tipo,
+        registro:car.registro
+    })
+
+    await use.save()
+    const userId = await vehiculo.findOne({
+        nombre:car.nombre    
+    },{_id:1})
+
+    await vehiculo.updateOne({
+        _id:userId._id.toString()    
+    },{$set:{id:userId._id.toString()}})
+
+    return "vehiculo fue registrado"
+
+}
+
+module.exports= {
+    crearVehiculo
+};
